b7fde5965e63f587bb2a409212632431dad4bab0
YaoerWu <honey_yolk@outlook.com>
2026-05-20 00:07:46 +0800

    feat: 支持急速战线活动

--- examples/event.py
+++ examples/event.py
@@ -1,14 +1,14 @@
-from autowsgr.fight.event.event_2026_0212 import EventFightPlan20260212
+from autowsgr.fight.event.event20260515 import EventFightPlan
 from autowsgr.scripts.main import start_script
 
 
 timer = start_script('./user_settings.yaml')
 # set_support(timer,True) # 如果要在战斗前开启战役支援请取消这一行的注释
-plan = EventFightPlan20260212(
+plan = EventFightPlan(
     timer,
-    plan_path='E5A夜战',
+    plan_path='E1炸鱼',
     fleet_id=2,
-)  # 修改E11BC夜战为相对于的plan，详细的plan名可在data/plans/event/20260212查看，fleet_id为出击编队
+)
 
 
 plan.run_for_times(
