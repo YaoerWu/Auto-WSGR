56f3e26dce5a4cca07ea069c7c321574e8639797
YaoerWu <honey_yolk@outlook.com>
2026-08-03 13:27:44 +0800

    更新 autowsgr-native 依赖版本至 0.3.0，并修正 ApiDll 类中的方法引用

--- autowsgr/timer/backends/api_dll.py
+++ autowsgr/timer/backends/api_dll.py
@@ -1,13 +1,13 @@
-import autowsgr_native
 import numpy as np
+from autowsgr_native.recognition import locate, recognize_enemy, recognize_map
 
 
 class ApiDll:
-    def locate(self, image: np.ndarray) -> list[tuple[int, int]]:
-        return autowsgr_native.locate(image)
+    def locate(self, image: np.ndarray) -> list[list[int]]:
+        return locate(image)
 
     def recognize_enemy(self, images: list[np.ndarray]) -> str:
-        return autowsgr_native.recognize_enemy(images)
+        return recognize_enemy(images)
 
     def recognize_map(self, image: np.ndarray) -> str:
-        return autowsgr_native.recognize_map(image)
+        return recognize_map(image)
